7e7124484bd1b79f6ea0bc1903fc8612e8945d37
cubeiplkumar <emailanilkumarm@gmail.com>
2024-06-11 09:23:15 +0530

    feature202 changes

--- Application.java
+++ Application.java
@@ -18,7 +18,37 @@ public class AllocationsApplication implements SampleApplication {
         }
         return true;
     }
+#feature202 changes by Developer Shilpa in Oct 2023
+ private class OddThread extends Thread {
 
+        public OddThread() {
+            super("Odd-Thread");
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < count; i++) {
+                if (!isEven(i)) {
+                    printNumber(i);
+                }
+            }
+        }
+    }
+
+
+    private void printNumber(int i) {
+        System.out.format("Thread: %s, Number: %d%n", Thread.currentThread().getName(), i);
+    }
+
+    private synchronized boolean isEven(int i) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return i % 2 == 0;
+    }
+#end of feature202
     @Override
     public void start() {
         Long primeCount = 0L;
